c2fc30290f7e8c80556e73a3ddf07da9a0fbf556
unknown <gagas112212@gmail.com>
2024-02-29 20:26:51 +0700

    transaction migration

--- database/migrations/2024_02_29_132441_create_transactions_table.php
+++ database/migrations/2024_02_29_132441_create_transactions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,14 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger("users_id");
+            $table->text("address")->nullable();
+            $table->float("total_price")->default(0);
+            $table->float("shipping_price")->default(0);
+            $table->string("status")->default("PENDING");
+            $table->string("payment")->default("MANUAL");
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
