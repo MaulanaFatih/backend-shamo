c7b9b4b90a0846510cdff44ff1fdf0f572b51dc5
unknown <gagas112212@gmail.com>
2024-03-01 11:07:49 +0700

    feat:fetch user API

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Fortify\Rules\Password;
 
@@ -46,5 +47,43 @@ class UserController extends Controller
             ], 'Register User Failed', 500);
         }
         return 'kelaz';
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => ['required'],
+                'password' => ['required'],
+            ]);
+
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthenticated',
+                ], 'Auth Failed', 500);
+            }
+            $user = User::where('email', $request->email)->first();
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception("invalid Credentials");
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Authenticated');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication User Failed', 500);
+        }
+    }
+
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success($request->user(), 'User profile');
     }
 }
